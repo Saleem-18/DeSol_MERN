aaf6df0d59eb4cfdd93aa337b13f2b9589e9b1be
DESKTOP-LKPGNK2\asq <saleemullah8818@gmail.com>
2024-12-05 17:06:14 +0500

    final changes

--- car-selling-backend/s3.js
+++ car-selling-backend/s3.js
@@ -2,9 +2,10 @@ const AWS = require("aws-sdk");
 require("dotenv").config();
 
 const s3 = new AWS.S3({
+  endpoint: "https://s3.eu-north-1.amazonaws.com",
+  region: "eu-north-1",
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
 });
 
 module.exports = s3;
